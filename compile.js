315fd80e277e8f2a57a324aa859014f87fc4050f
ramiro <ramihertz@gmail.com>
2019-05-13 07:33:23 -0400

    ok

---
+++ compile.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const source = fs.readFileSync(inboxPath, 'utf8');
+
+//let c = solc.compile(source, 1);
+console.log(solc.compile(source, 1));
